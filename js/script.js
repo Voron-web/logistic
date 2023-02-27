280d275043966fd431c346a541ba6d8583cc6787
Artem Voron <voron.web@disroot.org>
2023-02-27 16:07:07 +0300

    add change font size script

--- js/script.js
+++ js/script.js
@@ -320,4 +320,51 @@ document.addEventListener('DOMContentLoaded', () => {
             star.addEventListener('pointermove', startAnimation);
         }, 9000);
     }
+
+    //===============================================================================
+
+    //title font size
+
+    const container = document.querySelector('.container');
+    const titlePage = document.querySelector('h1');
+    let fontSize = 100;
+    titlePage.style.fontSize = fontSize + 'px';
+
+
+    function changeFontSize() {
+        let containerPadding = Number(window.getComputedStyle(container).paddingLeft.replace('px', '')) + Number(window.getComputedStyle(container).paddingLeft.replace('px', ''))
+
+        if (titlePage.clientWidth < container.clientWidth - containerPadding - 1) {
+            while (titlePage.clientWidth < (container.clientWidth - 1 - containerPadding)) {
+                if (titlePage.clientWidth < (container.clientWidth - containerPadding - 200)) {
+                    fontSize += 10;
+                } else {
+                    fontSize++;
+                }
+                titlePage.style.fontSize = fontSize + 'px';
+                if (fontSize > 500) {
+                    titlePage.style.fontSize = 50 + 'px';
+                    break;
+                }
+            }
+        }
+        else if (titlePage.clientWidth > container.clientWidth - containerPadding - 1) {
+            while (titlePage.clientWidth > (container.clientWidth - 1 - containerPadding)) {
+                if (titlePage.clientWidth > (container.clientWidth - containerPadding + 200)) {
+                    fontSize -= 10;
+                } else {
+                    fontSize--;
+                }
+                titlePage.style.fontSize = fontSize + 'px';
+                if (fontSize < 20) {
+                    titlePage.style.fontSize = 20 + 'px';
+                    break;
+                }
+            }
+        }
+    }
+
+    changeFontSize();
+    window.addEventListener('resize', changeFontSize);
+
 });
